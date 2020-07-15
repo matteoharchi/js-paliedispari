7ac41d2efa3f8cafbca0e8de81e22903fe9204e6
Matteo Harchi <matteo.harchi@gmail.com>
2020-07-15 15:31:26 +0200

    Finito con funzione

--- js/script.js
+++ js/script.js
@@ -3,24 +3,42 @@
 // è palindroma
 
 // Senza funzione
-var parola = prompt("Inserisci una parola e scopri se è palindroma");
-var parolaScomposta = [];
-for (var i = 0; i < parola.length; i++) {
-    parolaScomposta.push(parola[i]);
-}
-var alorapScomposta = parolaScomposta.reverse();
-var alorap = "";
-for (var j = 0; j < alorapScomposta.length; j++) {
-    alorap += alorapScomposta[j];
-}
-if (alorap == parola) {
-    alert("La parola è palindroma!")
-} else {
-    alert("La parola non è palindroma!")
-}
+// var parola = prompt("Inserisci una parola e scopri se è palindroma");
+// var parolaScomposta = [];
+// for (var i = 0; i < parola.length; i++) {
+//     parolaScomposta.push(parola[i]);
+// }
+// var alorapScomposta = parolaScomposta.reverse();
+// var alorap = "";
+// for (var j = 0; j < alorapScomposta.length; j++) {
+//     alorap += alorapScomposta[j];
+// }
+// if (alorap == parola) {
+//     alert("La parola è palindroma!")
+// } else {
+//     alert("La parola non è palindroma!")
+// }
 
 // con funzione
 
-function palindromia(parolaccia) {
+var mot = prompt("Inserisci una parola e scopri se è palindroma!");
+palindromia(mot);
 
+
+function palindromia(parolaccia) {
+    var palindromia = "";
+    var parolacciaScomposta = [];
+    for (var i = 0; i < parolaccia.length; i++) {
+        parolacciaScomposta.push(parolaccia[i]);
+    }
+    var parlacciaScompostaReverse = parolacciaScomposta.reverse();
+    var parolacciaReverse = "";
+    for (var i = 0; i < parolacciaScomposta.length; i++) {
+        parolacciaReverse += parolacciaScomposta[i];
+    }
+    if (parolaccia == parolacciaReverse) {
+        alert("La parola è palindroma!");
+    } else {
+        alert("La parola non è palindroma!");
+    }
 }
